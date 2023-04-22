46060225f46814011b48dead44dd7a6ee83c70ac
mflo <ragn3003@outlook.com>
2023-04-22 15:41:20 +0500

    head stable 20k

--- Assets/Environments/Head/HeadAgent.cs
+++ Assets/Environments/Head/HeadAgent.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 using Random = UnityEngine.Random;
 public class HeadAgent : MonoBehaviour
 {
-    
+
     public SRLAgent rLAgent => SRLAgent.me;
 
     public float delay = 1, startDelay;
@@ -15,15 +15,15 @@ public class HeadAgent : MonoBehaviour
     public int episodeCount, maxEpisodeCount;
     public float force;
     private Transition _Transition = new Transition();
-    
-    
+
+
     public Rigidbody rb;
     public Rigidbody ballRB;
     public HeadEnv env;
     public Transform point;
     public float randomRot = 10;
     public float epsilon;
-    
+
     private void Awake()
     {
         startDelay = delay;
@@ -58,8 +58,10 @@ public class HeadAgent : MonoBehaviour
         current_state.Clear();
 
 
-        AddObservation(rb.transform.up.normalized.x);
-        AddObservation(rb.transform.up.normalized.x);
+        AddObservation(rb.transform.rotation.x);
+        AddObservation(rb.transform.rotation.y);
+        AddObservation(rb.transform.rotation.z);
+        AddObservation(rb.transform.rotation.w);
         AddObservation(rb.angularVelocity.x);
         AddObservation(rb.angularVelocity.y);
         AddObservation(rb.angularVelocity.z);
@@ -75,15 +77,15 @@ public class HeadAgent : MonoBehaviour
         if (prev_state.Count == 0)
             Utils.CopyTo(current_state, prev_state);
 
-        Vector3 center = new Vector3(transform.position.x,0, transform.position.z);
-        Vector3 ballCenter = new Vector3(ballRB.transform.position.x,0, ballRB.transform.position.z);
+        Vector3 center = new Vector3(transform.position.x, 0, transform.position.z);
+        Vector3 ballCenter = new Vector3(ballRB.transform.position.x, 0, ballRB.transform.position.z);
         float distance = Vector3.Distance(point.position, ballRB.transform.position);
         float vel = ballRB.velocity.magnitude;
         //print("distance : " + distance);
-        float s_reward = 0.1f-0.1f*distance-0.1f*vel;
-        
+        float s_reward = reward - 0.1f * distance - 2*vel;
 
-        
+
+
         _Transition.Set(prev_state.ToArray(), action, current_state.ToArray(), s_reward);
         Utils.CopyTo(current_state, prev_state);
 
@@ -94,11 +96,19 @@ public class HeadAgent : MonoBehaviour
         }
 
 
-        action = rLAgent.SelectAction(prev_state.ToArray(),epsilon);
+        action = rLAgent.SelectAction(prev_state.ToArray(), epsilon);
+        //if (Input.GetKey(KeyCode.UpArrow))
+        //    action = 1;
+        //if (Input.GetKey(KeyCode.DownArrow))
+        //    action = 0;
+        //if (Input.GetKey(KeyCode.LeftArrow))
+        //    action = 2;
+        //if (Input.GetKey(KeyCode.RightArrow))
+        //    action = 3;
         MakeAction(action);
-        
+
         rLAgent.Learn(_Transition);
-        
+
         episodeCount++;
         if (maxrew < s_reward)
         {
@@ -106,23 +116,23 @@ public class HeadAgent : MonoBehaviour
             //print("maxrew : " + maxrew);
             rLAgent.ReplaceTarget();
         }
-        
+
         print("reward : " + s_reward);
 
-        if(_Transition.isDone)
+        if (_Transition.isDone)
             Restart();
     }
     void MakeAction(int action)
     {
         // 0  1  2  3
         // -x x -z  z
-        float x = 0,z = 0;
+        float x = 0, z = 0;
         switch (action)
-        { 
-            case 0: x = -force;break;
-            case 1: x = force;break;
-            case 2: z = force;break;
-            case 3: z = -force;break;
+        {
+            case 0: x = -force; break;
+            case 1: x = force; break;
+            case 2: z = force; break;
+            case 3: z = -force; break;
         }
         rb.AddTorque(x, 0, z);
     }
